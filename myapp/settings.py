f5ccb43775be4830336a6588fa72118eac8f9f03
Theivaraj-dev <theivaraj777@gmail.com>
2025-03-31 18:55:48 +0530

    python version update

--- myapp/settings.py
+++ myapp/settings.py
@@ -142,22 +142,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-        },
-    },
-    "root": {
-        "handlers": ["console"],
-        "level": "DEBUG",
-    },
-}
-
-
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
