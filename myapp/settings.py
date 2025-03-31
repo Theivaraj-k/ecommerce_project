f373f967e103d55fc93cad054d13ac0350801e5b
Theivaraj-dev <theivaraj777@gmail.com>
2025-03-31 16:09:08 +0530

    change  file add

--- myapp/settings.py
+++ myapp/settings.py
@@ -83,12 +83,7 @@ DATABASES = {
         'PASSWORD':'12345',
         'HOST':'localhost',
         'PORT':'3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-
-
-             
-        }
+    
     }
 }
 
